993d9bbeddf409c4ea7f8df41ac208935e46c7ec
João Gabriel de Paula <jgp.depaula@gmail.com>
2021-10-19 20:56:00 -0300

    Developed get person by ID

--- src/main/java/one/digitalInnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalInnovation/personapi/service/PersonService.java
@@ -40,11 +40,10 @@ public class PersonService {
     }
 
     public PersonDTO findById(Long id) throws PersonNotFoundException {
-        Optional<Person> optionalPerson = personRepository.findById(id);
-        if (optionalPerson.isEmpty()) {
-            throw new PersonNotFoundException(id);
-        }
-        return PersonMapper.INSTANCE.toDTO(optionalPerson.get());
+        Person person = personRepository.findById(id)
+                            .orElseThrow(() -> new PersonNotFoundException(id));
+       
+        return PersonMapper.INSTANCE.toDTO(person);
     }
 
 
